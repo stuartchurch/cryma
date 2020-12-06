b37d0b149ee3412f057ff73e52670111e24c9eab
Stuart Church <stuart.church@gmail.com>
2020-12-06 16:29:51 +0000

    Update cryma.py

--- cryma.py
+++ cryma.py
@@ -15,9 +15,9 @@ st.write("Choose moving averages to explore best buy/sell times")
 crypto = st.sidebar.selectbox('Select a Cryptocurrency',['BTC-GBP', 'ETH-GBP', 'XRP-GBP','BCH-GBP','LTC-GBP','XLM-GBP'])
 
 
-lowma = st.sidebar.slider("Low Moving Average",5,50,20)
-highma = st.sidebar.slider("High Moving Average",20,200,50)
-time = st.sidebar.slider("Time period (days)",100,1000,300)
+lowma = st.sidebar.slider("Low Moving Average",1,50,20)
+highma = st.sidebar.slider("High Moving Average",10,200,50)
+time = st.sidebar.slider("Time period (days)",0,1000,300)
 freq = st.sidebar.radio('Frequency', ['1d','60m'])
 
 #append 'd' to time to define period for yf call
@@ -34,13 +34,11 @@ crypto = yf.Ticker(crypto)
 df = crypto.history(period = period, interval = freq)
 df.reset_index(inplace = True)
 
-
 st.write("Latest daily prices")
 st.dataframe(df.tail(1))
 
 SMAlow = df['Close'].rolling(window = lowma).mean()
 SMAhigh = df['Close'].rolling(window = highma).mean()
-
 
 matplotlib.rc('font', size='6')
 
